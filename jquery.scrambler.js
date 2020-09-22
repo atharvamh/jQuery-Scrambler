be704dbe21b39b73c427b9ac8d518bbb89352539
Atharva <46068146+atharvamh@users.noreply.github.com>
2020-09-22 13:46:11 +0530

    Update jquery.scrambler.js

--- jquery.scrambler.js
+++ jquery.scrambler.js
@@ -1,10 +1,8 @@
 /*
  * Scrambler: jQuery Plugin for scrambling text
- *
  * @author: @atharvamh
  * @version: 1.0.0
  * @url: https://github.com/atharvamh/jQuery-Scrambler
- *
  */
 
 ;(function($) {
@@ -16,42 +14,34 @@
 
         var $element = $(element),
         element = element;
-
-
+        
         var defaults = {
-            effect : "charbychar", // 2 options - charbychar and typewriter
+            effect : "charbychar", // 2 options - charbychar and typing
             keep_whitespaces : true, // true: keep whitespaces, false: fill whitespaces with a random character
-            speed : 100, // speed in which random chars will appear in letters not yet revelaed
+            speed : 100,
             duration : 3000,
             final_text: $element.text(),
-            reveal: 1000, // number of milliseconds to reveal the character
+            reveal: 1000, // number of milliseconds
             total_iterations : 0,
             interval : -1
         }
 
         plugin.init = function() {
-
             plugin.settings = $.extend({}, defaults, options);
-
             var el = $element;
             var str = plugin.settings.final_text;
 
-
-
-            if (
-              plugin.settings.effect == "charbychar" || plugin.settings.effect == "typewriter"
-            ) {
-              var pos_limit = 0;            // randomize text from this position to the end
+            if (plugin.settings.effect == "charbychar" || plugin.settings.effect == "typing"){
+              var pos_limit = 0;                                             // randomize text from this position to the end
               var pos_total = str.length;
               var internal_char_reveal_counter = 0;
-              plugin.settings.duration = pos_total * plugin.settings.reveal; // duration is auto calculated with the reveal value
+              plugin.settings.duration = pos_total * plugin.settings.reveal; // calculating duration with reveal
 
               plugin.settings.interval = setInterval( function(){
 
-                  // char changer effect
-                  el.text( scrambler_char_by_char( str, pos_limit,  plugin.settings.effect) );
+                  // changing text character by character effect
+                  el.text( scrambler_char_by_char( str, pos_limit, plugin.settings.effect) );
 
-                  // time controller
                   plugin.settings.total_iterations += plugin.settings.speed;
                   internal_char_reveal_counter += plugin.settings.speed;
 
@@ -60,6 +50,7 @@
                     internal_char_reveal_counter = 0;
                     pos_limit++;
                   }
+                  
                   // end and stop interval
                   if ( plugin.settings.total_iterations >= plugin.settings.duration ) {
                       clearInterval(plugin.settings.interval);
@@ -67,12 +58,8 @@
                       plugin.settings.interval = -1;
                       el.text(str);
                   }
-
-
               }, plugin.settings.speed );
-
             }
-
         }
 
 
@@ -81,7 +68,6 @@
         var scramble_get_random_char = function (max, min){
             var random = Math.floor(Math.random() * (max - min + 1) + min);
             return String.fromCharCode(random);
-
         }
 
         /* scramble and reveal characters one by one */
@@ -97,24 +83,16 @@
                 ret_string += scramble_get_random_char(33, 126);
               }
           }
-          if (effect == "typewriter") ret_string = "|";
+          if (effect == "typing") ret_string = "|";
           return fixed_chars + ret_string;
         }
-
-
         plugin.init();
-
     }
 
     $.fn.scrambler = function(options) {
-
         return this.each(function() {
-
             var plugin = new $.scrambler(this, options);
             $(this).data('scrambler', plugin);
-
         });
-
     }
-
 })(jQuery);
